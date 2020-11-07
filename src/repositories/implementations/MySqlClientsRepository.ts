da9d468fbc96198dab683d0e0b023c440454b7c4
aleksandersousa <aleksander-ssousa@outlook.com>
2020-11-07 16:38:53 -0300

    Added database interface and mysql connection

--- src/repositories/implementations/MySqlClientsRepository.ts
+++ src/repositories/implementations/MySqlClientsRepository.ts
@@ -1,33 +1,15 @@
 import { Client } from 'src/entities/Client'
 import { IClientRepository } from '../IClientRepository'
-import { db } from '../../db'
+import { MySqlDatabase } from '../../config/MysqlDatabase'
 
 export class MySqlClientsRepository implements IClientRepository {
-  async findByEmail (email: string): Promise<Client> {
-    const getClient = new Promise((resolve, reject) => {
-      db.query('SELECT * FROM client WHERE email = ?', [email], (error, results) => {
-        if (error) throw error
-        if (results.length > 0) {
-          resolve(results[0])
-        }
-        return resolve(null)
-      })
-    })
+  private mysqlDatabase = new MySqlDatabase()
 
-    return getClient.then(function (results) {
-      if (results) {
-        return new Client(results)
-      }
-      return null
-    }).catch((err) => {
-      console.log('Error on database insert client querie: ' + err)
-      return null
-    })
+  async findByEmail (email: string): Promise<Client> {
+    return await this.mysqlDatabase.findByEmail(email)
   }
 
   async save (client: Client): Promise<void> {
-    db.query('INSERT INTO client SET ? ', client, (error, results) => {
-      if (error) throw error
-    })
+    return await this.mysqlDatabase.save(client)
   }
 }
