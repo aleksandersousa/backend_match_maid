9e21f79ce5ca7d089dfa3bb02ddd8f6580a4501d
aleksandersousa <aleksander-ssousa@outlook.com>
2020-11-10 22:32:51 -0300

    Added db interface and mysql connection

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,5 @@
 import { app } from './app'
-import { db } from './db'
-
-db.connect((err) => {
-  if (err) {
-    console.log('Error connecting to db: ' + err)
-    return
-  }
-  console.log('connected to db')
-})
 
 app.listen(3000, () => {
-  console.log('backend rodando na porta 3333..')
+  console.log('backend running on port 3333...')
 })
