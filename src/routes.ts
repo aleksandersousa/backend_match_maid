f4a78ec140a402485c2bcfc3bad3cee10ab2aa5e
aleksandersousa <aleksander-ssousa@outlook.com>
2020-11-18 00:04:13 -0300

    created delete client method

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,20 @@
 import { Router } from 'express'
-import { createUserClientController } from './useCases/createClient'
-import { createUserMaidController } from './useCases/createMaid'
+import { createClientController } from './useCases/createClient'
+import { createMaidController } from './useCases/createMaid'
+import { deleteClientController } from './useCases/deleteClient'
 
 const router = Router()
 
 router.post('/create/client', (request, response) => {
-  return createUserClientController.handle(request, response)
+  return createClientController.handle(request, response)
 })
 
 router.post('/create/maid', (request, response) => {
-  return createUserMaidController.handle(request, response)
+  return createMaidController.handle(request, response)
+})
+
+router.delete('/delete/client', (request, response) => {
+  return deleteClientController.handle(request, response)
 })
 
 export { router }
