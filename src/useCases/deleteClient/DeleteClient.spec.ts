ded3bc1c7733c495db8bb5300129c237fd470e99
aleksandersousa <aleksander-ssousa@outlook.com>
2020-12-10 10:50:00 -0300

    Remove routes tests

--- src/useCases/deleteClient/DeleteClient.spec.ts
+++
@@ -1,9 +0,0 @@
-import request from 'supertest'
-import { app } from '../../app'
-
-describe('testing server routes', () => {
-  it('should delete client - DELETE/delete/client - success', async () => {
-    const { body } = await request(app).delete('/delete/client/1')
-    expect(body).toEqual({})
-  })
-})
